3e3a59ac119378fae258bab586109af0943b3f1a
Davi <davipatricio2006@gmail.com>
2021-11-20 09:54:56 -0300

    ✨ Expose more structures in Index

--- src/esm.mjs
+++ src/esm.mjs
@@ -1,8 +1,11 @@
 import Helly from './index.js';
 export const {
     Client,
+    ClientUser,
     Guild,
     GuildChannel,
     TextChannel,
+    MessageEmbed,
+    User,
     Utils,
 } = Helly;
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export { default as Client } from './client/Client';
+export { default as ClientUser } from './structures/ClientUser';
 export { default as Guild } from './structures/Guild';
 export { default as GuildChannel } from './structures/GuildChannel';
 export { default as TextChannel } from './structures/TextChannel';
-export { default as Utils } from './utils/index';
+export { default as MessageEmbed } from './structures/MessageEmbed';
+export { default as User } from './structures/User';
+export * as Utils from './utils/index';
