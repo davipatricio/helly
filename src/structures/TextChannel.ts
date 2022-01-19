f438f19b386cd321c91434393da1f47f264095d7
davipatricio <davipatricio2006@gmail.com>
2022-01-19 19:30:12 -0300

    [textchannel] create send method

--- src/structures/TextChannel.ts
+++ src/structures/TextChannel.ts
@@ -1,5 +1,7 @@
-import { Channel } from './Channel';
 import { ChannelType } from '../constants/channelTypes';
+import { Channel, MessageOptions } from './Channel';
+import { Message } from './Message';
+import { makeAPIMessage } from '../utils/MakeAPIMessage';
 
 import type { Guild } from './Guild';
 import type { Client } from '../client/Client';
@@ -9,6 +11,18 @@ class TextChannel extends Channel {
 	constructor(client: Client, data: any, guild?: Guild) {
 		super(client, data, guild);
 		this.parseData(data);
+	}
+
+	/**
+	 * Sends a message to this channel.
+	 * @param {string|MessagePayload} content
+	 * @returns {Promise<Message>}
+	 */
+	async send(content: MessageOptions) {
+		if (typeof content === 'string') content = { content };
+		const transformedObject = makeAPIMessage(content);
+		const data = await this.client.requester.make(`channels/${this.id}/messages`, 'POST', transformedObject);
+		return new Message(this.client, data);
 	}
 
 	override parseData(data: any) {
