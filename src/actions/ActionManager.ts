8acaac761a3e89e8e40f0a918c0ebd92d5d6e35a
davipatricio <davipatricio2006@gmail.com>
2022-01-24 11:13:18 -0300

    [Actions] add support to Guild Members Chunk

--- src/actions/ActionManager.ts
+++ src/actions/ActionManager.ts
@@ -9,6 +9,7 @@ class ActionManager {
 		this.loaded['READY'] = require('./READY');
 		this.loaded['GUILD_CREATE'] = require('./GUILD_CREATE');
 		this.loaded['GUILD_DELETE'] = require('./GUILD_DELETE');
+		this.loaded['GUILD_MEMBERS_CHUNK'] = require('./GUILD_MEMBERS_CHUNK');
 		this.loaded['MESSAGE_CREATE'] = require('./MESSAGE_CREATE');
 	}
 }
---
+++ src/actions/GUILD_MEMBERS_CHUNK.ts
@@ -0,0 +1,26 @@
+import type { Client } from '../client/Client';
+import { GuildMember } from '../structures/GuildMember';
+
+function handle(client: Client, chunkData: any) {
+	const guildId: string = chunkData.guild_id;
+	const guild = client.guilds.cache.get(guildId);
+	if (!guild) return;
+	const members: any[] = chunkData.members;
+	const chunkIndex: number = chunkData.chunk_index;
+	const chunkCount: number = chunkData.chunk_count;
+	const nonce: string = chunkData.nonce;
+
+	const _parsedMembers: Map<string, GuildMember> = new Map();
+	for (const member of members) {
+		const _member = new GuildMember(client, member, guild);
+
+		client.users.cache.set(_member.user.id, _member.user);
+		guild.members.cache.set(_member.user.id, _member);
+
+		_parsedMembers.set(_member.user.id, _member);
+	}
+
+	client.emit('guildMembersChunk', _parsedMembers, guild, { chunkIndex, chunkCount, nonce });
+}
+
+export { handle };
