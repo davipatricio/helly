fa1a20057307577b2d6225236147690daf9ff715
davipatricio <davipatricio2006@gmail.com>
2022-01-24 11:11:19 -0300

    [Snowflake] add generate method

--- src/utils/Snowflake.ts
+++ src/utils/Snowflake.ts
@@ -1,4 +1,5 @@
 const epoch = new Date('2015-01-01T00:00:00.000Z').getTime();
+let INCREMENT = BigInt(0);
 
 /**
  * Utility class for working with Snowflakes
@@ -15,6 +16,13 @@ class Snowflake extends null {
 		const bigIntSnowflake = BigInt(id);
 		return Number(bigIntSnowflake >> 22n) + epoch;
 	}
+	static generate(timestamp = Date.now() as number) {
+		if (INCREMENT >= 4095n) INCREMENT = BigInt(0);
+
+		// Assign WorkerId as 1 and ProcessId as 0:
+		return ((BigInt(timestamp - epoch) << 22n) | (1n << 17n) | INCREMENT++).toString();
+	}
+
 }
 
 export { Snowflake };
