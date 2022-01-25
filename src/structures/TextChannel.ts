205071c96511ce5cb753b30e0eb7470ce16e3465
davipatricio <davipatricio2006@gmail.com>
2022-01-25 13:41:44 -0300

    [TextChannel] add set{Topic,RateLimitPerUser} methods

--- src/structures/TextChannel.ts
+++ src/structures/TextChannel.ts
@@ -11,9 +11,35 @@ import { Message } from './Message';
  */
 class TextChannel extends Channel {
 	declare type: 'GUILD_TEXT';
+	topic!: string | null;
+	rateLimitPerUser!: number;
 	constructor(client: Client, data: any, guild?: Guild) {
 		super(client, data, guild);
 		this.parseData(data);
+	}
+
+	/**
+	 * Changes the topic of the channel
+	 * @param {?string} name - The new channel topic
+	 * @param {string} [reason] - The reason for changing the channel topic
+	 * @returns {Promise<TextChannel>}
+	 */
+	async setTopic(topic = null as string | null, reason?: string): Promise<this> {
+		const data = await this.client.requester.make(`channels/${this.id}`, 'PATCH', { topic }, { 'X-Audit-Log-Reason': reason });
+		this.parseData(data);
+		return this;
+	}
+
+
+	/**
+	 * Changes the channel slowmode
+	 * @param {number} [seconds=0] - New slowmode duration
+	 * @param {string} [reason] - Reason for changing the channel slowmode
+	 */
+	async setRateLimitPerUser(seconds = 0 as number, reason?: string) {
+		const data = await this.client.requester.make(`channels/${this.id}`, 'PATCH', { seconds }, { 'X-Audit-Log-Reason': reason });
+		this.parseData(data);
+		return this;
 	}
 
 	/**
@@ -43,6 +69,22 @@ class TextChannel extends Channel {
 	override parseData(data: any) {
 		if (typeof data === 'undefined') return null;
 		super.parseData(data);
+
+		if ('topic' in data) {
+			/**
+			 * The topic of the text channel
+			 * @type {?string}
+			 */
+			this.topic = data.topic;
+		}
+
+		if ('rate_limit_per_user' in data) {
+			/**
+			 * The rate limit per user (slowmode) for this channel in seconds
+			 * @type {number}
+			 */
+			this.rateLimitPerUser = data.rate_limit_per_user;
+		}
 	}
 }
 
