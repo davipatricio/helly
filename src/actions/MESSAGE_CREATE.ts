b5b87a2af126e4cbc919d6e32103e426712b7b8b
davipatricio <davipatricio2006@gmail.com>
2022-01-25 21:21:27 -0300

    oopsie, this shouldnt happen

--- src/actions/MESSAGE_CREATE.ts
+++ src/actions/MESSAGE_CREATE.ts
@@ -1,16 +1,9 @@
 import { Message } from '../structures/Message';
 import type { Client } from '../client/Client';
-import type { TextChannel } from '../structures/TextChannel';
 
 function handle(client: Client, messageData: any): void {
 	if (client.ready) {
 		const message = new Message(client, messageData);
-		const channel = client.channels.cache.get(message.channelId);
-
-		if (channel) {
-			if (!channel.isTextBased()) return;
-			(channel as TextChannel).messages.cache.set(message.id, message);
-		}
 
 		/**
 		 * Emitted when a message is sent.
--- src/managers/ChannelManager.ts
+++ src/managers/ChannelManager.ts
@@ -1,12 +1,9 @@
 import type { Client } from '../client/Client';
 import type { Channel } from '../structures/Channel';
-import type { TextChannel } from '../structures/TextChannel';
 import { LimitedMap } from '../utils/LimitedMap';
 
-export type AnyChannel = TextChannel | Channel;
-
 class ChannelManager {
-	cache: LimitedMap<string, AnyChannel>;
+	cache: LimitedMap<string, Channel>;
 	client: Client;
 	constructor(client: Client, limit: number) {
 		this.cache = new LimitedMap(limit);
