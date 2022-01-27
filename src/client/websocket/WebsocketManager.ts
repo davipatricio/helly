799b03fb7ff8d7055044721041d34256aeb921e5
davipatricio <davipatricio2006@gmail.com>
2022-01-27 08:28:31 -0300

    [WebsocketManager] reduce complexity

--- src/client/websocket/WebsocketManager.ts
+++ src/client/websocket/WebsocketManager.ts
@@ -3,6 +3,12 @@ import { apiGatewayUrl, apiVersion } from '../../constants/gateway';
 import type { Client } from '../Client';
 import { stop } from './Heartbeater';
 import Parser from './Parser';
+import { closeCodeMessages } from '../../constants/wsCloseCodes';
+
+const closeCodesAbleToResume = [4000, 4002, 4003];
+const closeCodesAbleToReconnect = [4001, 4005, 4007, 4008, 4009];
+const closeCodesToThrow = [4004, 4010, 4011, 4012, 4013, 4014];
+type CloseCode = keyof typeof closeCodeMessages;
 
 class WebsocketManager {
 	client: Client;
@@ -17,62 +23,24 @@ class WebsocketManager {
 
 		this.connection.on('close', (code: number) => {
 			if (!this.client.options.autoReconnect) return;
-			switch (code) {
-			case 4000:
-				this.client.emit('debug', '[DEBUG] Unknown websocket error! Resuming...');
-				this.forceReconnect();
-				break;
-			case 4001:
-				this.client.emit('debug', '[DEBUG] We have sent an unknwon or invalid opcode to Discord. Reconnecting...');
-				this.forceReconnect(false);
-				break;
-			case 4002:
-				this.client.emit('debug', '[DEBUG] We have sent an invalid payload to Discord. Reconnecting...');
-				this.forceReconnect();
-				break;
-			case 4003:
-				this.client.emit('debug', '[DEBUG] We have sent a payload to Discord before connecting. Resuming...');
-				this.forceReconnect();
-				break;
-			case 4004:
-				this.client.emit('debug', '[DEBUG] The provided account token is incorrect, NOT attempting to reconnect.');
-				throw new Error('DiscordError: Invalid token');
-			case 4005:
-				this.client.emit('debug', '[DEBUG] We have tried to send more than one identify payload. Reconnecting...');
-				this.forceReconnect(false);
-				break;
-			case 4007:
-				this.client.emit('debug', '[DEBUG] We have provided an invalid sequence to Discord. Reconnect...');
-				this.forceReconnect(false);
-				break;
-			case 4008:
-				this.client.emit('debug', '[DEBUG] Unexpected Rate Limit! Reconnecting in 3 seconds...');
-				setTimeout(() => this.forceReconnect(false), 3000).unref();
-				break;
-			case 4009:
-				this.client.emit('debug', '[DEBUG] The bot session timed out. Reconnecting and starting a new one.');
-				this.forceReconnect(false);
-				break;
-			case 4010:
-				this.client.emit('debug', '[DEBUG] You have provided invalid sharding data, NOT attempting to reconnect.');
-				throw new Error('DiscordError: Invalid shard');
-			case 4011:
-				this.client.emit('debug', '[DEBUG] The session would have handled too many guilds - you are required to shard your connection in order to connect.');
-				throw new Error('DiscordError: 	Sharding required');
-			case 4012:
-				this.client.emit('debug', '[DEBUG] You have provided an invalid API versiion, NOT attempting to reconnect.');
-				throw new Error('DiscordError: Invalid API version');
-			case 4013:
-				this.client.emit('debug', '[DEBUG] Invalid intents provided, NOT attempting to reconnect.');
-				throw new Error('DiscordError: Invalid intent(s)');
-			case 4014:
-				this.client.emit('debug', '[DEBUG] You sent a disallowed intent for a Gateway Intent, NOT attempting to reconnect.');
-				throw new Error('DiscordError: Disallowed intent(s)');
-			default:
-				this.forceReconnect(false);
-				break;
-			}
+			if (typeof code !== 'number') return;
+			this._parseClodeCode(code as CloseCode);
 		});
+	}
+
+	_parseClodeCode(code: CloseCode) {
+		let _foundCode = false;
+		if(closeCodesAbleToResume.includes(code)) {
+			_foundCode = true;
+			this.forceReconnect(true);
+		}
+		if(closeCodesAbleToReconnect.includes(code)) {
+			_foundCode = true;
+			this.client.api.sessionId = null;
+			this.forceReconnect(false);
+		}
+		if(closeCodesToThrow.includes(code) && !_foundCode) throw new Error(`DiscordError: ${closeCodeMessages[code]} ${code}`);
+		this.client.emit('debug', `[DEBUG] ${closeCodeMessages[code] ?? 'Websocket connection closed with unknown close code. Reconnecting instead of resuming...'}`);
 	}
 
 	forceReconnect(resume = true) {
