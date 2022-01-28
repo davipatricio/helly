8b961dab0a7b537d58909ff897d853ca18a6e9df
davipatricio <davipatricio2006@gmail.com>
2022-01-27 21:20:18 -0300

    [constants] export intents & WSCloseCodes

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,5 @@ export * as Gateway from './gateway';
 export * as Images from './images';
 export * as ChannelTypes from './channelTypes';
 export * as Permissions from './permissions';
+export * as Intents from './intents';
+export * as WSCloseCodes from './wsCloseCodes';
