6785d589f9246c9fe00ad78afa2b9000593ecd27
davipatricio <davipatricio2006@gmail.com>
2022-03-06 10:02:25 -0300

    check if guild wasnt available to the client in `GuildCreate`

--- src/client/actions/GUILD_CREATE.ts
+++ src/client/actions/GUILD_CREATE.ts
@@ -4,7 +4,15 @@ import type { Guild } from '../../structures/Guild';
 import type { Client } from '../Client';
 
 function handle(client: Client, data: GatewayGuildCreateDispatchData) {
+  const oldGuild = client.caches.guilds.get(data.id);
   const guild = client.guilds.updateOrSet(data.id, data);
+
+  // When a Guild becomes available again to the client
+  if (oldGuild) {
+    if (!guild.available && !data.unavailable) return;
+  }
+
+  // When the current user joins a new Guild
   if (client.ready) client.emit(Events.GuildCreate, guild as Guild);
 }
 
--- src/client/actions/GUILD_DELETE.ts
+++ src/client/actions/GUILD_DELETE.ts
@@ -5,12 +5,15 @@ import type { Client } from '../Client';
 
 function handle(client: Client, data: GatewayGuildDeleteDispatchData) {
   const guild = client.caches.guilds.get(data.id);
+
   if (guild) {
+    // When a guild becomes or was already unavailable due to an outage
     if (data.unavailable) {
       guild.data.unavailable = true;
       client.emit(Events.GuildUnavailable, guild);
       return;
     }
+
     if (client.ready) client.emit(Events.GuildDelete, guild as Guild);
 
     for (const channel of guild.channels.cache.values()) client.caches.channels.delete(channel.id);
