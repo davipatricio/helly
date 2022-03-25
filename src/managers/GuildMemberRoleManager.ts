c1af7acd16450c49379d027263f08d8ebed1fcfa
davipatricio <davipatricio2006@gmail.com>
2022-03-25 18:58:25 -0300

    feat(GuildMemberRoleManager): create `add` & `remove` methods

--- src/managers/GuildMemberRoleManager.ts
+++ src/managers/GuildMemberRoleManager.ts
@@ -1,4 +1,5 @@
 import Collection from '@discordjs/collection';
+import { Routes } from 'discord-api-types/v10';
 import type { Client } from '../client/Client';
 import type { Guild } from '../structures/Guild';
 import type { GuildMember } from '../structures/GuildMember';
@@ -45,7 +46,63 @@ class GuildMemberRoleManager {
    * ```
    */
   set(roles: string | string[] = [], reason = '') {
-    return this.guild.members.edit(this.member.id as string, { roles: Array.isArray(roles) ? roles : [roles] }, reason);
+    return this.guild.members.edit(this.member.id, { roles: Array.isArray(roles) ? roles : [roles] }, reason);
+  }
+
+  /**
+   * Adds a role (or multiple roles) from the member.
+   * @param roles The role ids to add
+   * @param reason Reason for adding the roles
+   * @example
+   * ```js
+   * // Add a single role to the member
+   * guildMember.roles.add('391156570408615936')
+   * ```
+   * @example
+   * ```js
+   * // Add multiple roles to the member
+   * guildMember.roles.add(['391156570408615936', '391156570408615937'])
+   * ```
+   */
+  async add(roles: string | string[] = [], reason = '') {
+    if (roles instanceof Collection || Array.isArray(roles)) {
+      const finalRoles: string[] = [];
+      for (const role of roles.values()) finalRoles.push(role);
+
+      const newRoles = [...new Set(finalRoles.concat(...this.cache.keys()))];
+      return this.guild.members.edit(this.member.id, { roles: newRoles }, reason);
+    }
+
+    await this.client.rest.make(Routes.guildMemberRole(this.guild.id, this.member.id, roles), 'Put', undefined, { 'X-Audit-Log-Reason': reason });
+    return undefined;
+  }
+
+  /**
+   * Removes a role (or multiple roles) from the member.
+   * @param roles The role ids to remove
+   * @param reason Reason for removing the roles
+   * @example
+   * ```js
+   * // Remove a single role from the member
+   * guildMember.roles.remove('391156570408615936')
+   * ```
+   * @example
+   * ```js
+   * // Remove multiple roles from the member
+   * guildMember.roles.remove(['391156570408615936', '391156570408615937'])
+   * ```
+   */
+  async remove(roles: string | string[] = [], reason = '') {
+    if (roles instanceof Collection || Array.isArray(roles)) {
+      const finalRoles: string[] = [];
+      for (const role of roles.values()) finalRoles.push(role);
+
+      const newRoles = [...this.cache.filter(role => !finalRoles.includes(role.id)).keys()];
+      return this.guild.members.edit(this.member.id, { roles: newRoles }, reason);
+    }
+
+    await this.client.rest.make(Routes.guildMemberRole(this.guild.id, this.member.id, roles), 'Delete', undefined, { 'X-Audit-Log-Reason': reason });
+    return undefined;
   }
 }
 
