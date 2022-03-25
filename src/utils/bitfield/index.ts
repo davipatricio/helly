328fe035e6915a39d1d4f7cb6be6fb7d830c68bc
davipatricio <davipatricio2006@gmail.com>
2022-03-25 14:14:43 -0300

    feat: SystemChannelFlagsBitField

--- src/utils/bitfield/index.ts
+++ src/utils/bitfield/index.ts
@@ -1,4 +1,5 @@
 export * from './IntentsBitField';
 export * from './MessageFlagsBitField';
 export * from './PermissionsBitField';
+export * from './SystemChannelFlagsBitField';
 export * from './UserFlagsBitField';
