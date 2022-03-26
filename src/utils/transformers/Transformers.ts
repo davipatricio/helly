a1c6dd3b73e1873da4edc6c252f6be8a6679bb45
Niskii3 <psychoniskii2@gmail.com>
2022-03-26 14:49:12 -0300

    feat(Transformers): add `banOptions` method

--- src/utils/transformers/Transformers.ts
+++ src/utils/transformers/Transformers.ts
@@ -12,10 +12,12 @@ import type {
   APITextChannel,
   APITextInputComponent,
   MessageFlags,
+  RESTPutAPIGuildBanJSONBody,
 } from 'discord-api-types/v10';
 import { Embed } from '../../builders/Embed';
 import type { ChannelData, MessageReferenceSend } from '../../structures/Channel';
 import type { Guild, GuildWidgetSettingsData } from '../../structures/Guild';
+import type { BanOptions } from '../../structures/GuildMember';
 import { MessageFlagsBitField } from '../bitfield/MessageFlagsBitField';
 import { SystemChannelFlagsBitField } from '../bitfield/SystemChannelFlagsBitField';
 import { Parsers } from './Parsers';
@@ -101,6 +103,15 @@ class Transformers extends null {
     if (!data) return undefined;
     if (data instanceof ActionRowBuilder) return data.toJSON();
     return data;
+  }
+
+  static banOptions(): undefined;
+  static banOptions(data?: BanOptions): RESTPutAPIGuildBanJSONBody;
+  static banOptions(data?: BanOptions) {
+    if (!data) return undefined;
+    return {
+      delete_message_days: data.deleteMessageDays,
+    } as RESTPutAPIGuildBanJSONBody;
   }
 }
 
