beb9d6de83f793e52e9bf83ad6fa9e476e8d3899
davipatricio <davipatricio2006@gmail.com>
2022-03-26 15:09:11 -0300

    fix(Manager): add missing `MessageManager` export

--- src/managers/index.ts
+++ src/managers/index.ts
@@ -3,5 +3,7 @@ export * from './ChannelManager';
 export * from './GuildChannelManager';
 export * from './GuildManager';
 export * from './GuildMemberManager';
+export * from './MessageManager';
 export * from './RoleManager';
 export * from './UserManager';
+
