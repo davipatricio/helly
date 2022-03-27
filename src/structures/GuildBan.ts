7ccecb5aff17bc8bbaf4bdfea2ba440ccb9ce536
Nicolas Ribeiro <psychoniskii2@gmail.com>
2022-03-26 21:21:50 -0300

    feat: add support to bans & member searching (#103)
    * feat: add support to bans & member searching
    
    * chore(ESLint): disable `no-restricted-globals` rule
    
    Co-authored-by: Davi Patricio <davipatricio@pm.me>

---
+++ src/structures/GuildBan.ts
@@ -0,0 +1,38 @@
+import type { APIBan } from 'discord-api-types/v10';
+import type { Client } from '../client/Client';
+import { BaseStructure } from './BaseStructure';
+import type { Guild } from './Guild';
+import { User } from './User';
+
+class GuildBan extends BaseStructure {
+  /** Raw ban data */
+  data: APIBan;
+  /** The guild in which the ban was issued */
+  guild: Guild;
+  constructor(client: Client, data: APIBan, guild: Guild) {
+    super(client);
+    this.guild = guild;
+    this.parseData(data);
+  }
+
+  /** The reason the user was banned */
+  get reason() {
+    return this.data.reason;
+  }
+
+  /** The user that was banned */
+  get user() {
+    return this.client.caches.users.get(this.data.user.id) ?? new User(this.client, this.data.user);
+  }
+
+  /** @private */
+  parseData(data: APIBan) {
+    if (!data) return this;
+
+    this.data = { ...this.data, ...data };
+
+    return this;
+  }
+}
+
+export { GuildBan };
