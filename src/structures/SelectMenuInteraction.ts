63dfd11e87a35743b28fa6a4c9a29baf28d59c83
davipatricio <davipatricio2006@gmail.com>
2022-04-01 20:42:12 -0300

    feat: SelectMenuInteraction

---
+++ src/structures/SelectMenuInteraction.ts
@@ -0,0 +1,290 @@
+import { APIMessageComponentSelectMenuInteraction, InteractionResponseType, MessageFlags, Routes } from 'discord-api-types/v10';
+import type { Client } from '../client/Client';
+import { Snowflake } from '../utils';
+import { MakeAPIMessage } from '../utils/rest';
+import { BaseStructure } from './BaseStructure';
+import type { MessageOptions } from './Channel';
+import type { InteractionDeferReplyOptions } from './ChatInputCommandInteraction';
+import type { Message } from './Message';
+import { Webhook } from './Webhook';
+
+class SelectMenuInteraction extends BaseStructure {
+  /** Raw {@link SelectMenuInteraction} data */
+  data: APIMessageComponentSelectMenuInteraction;
+  /** Whether the reply to this interaction is ephemeral */
+  ephemeral: boolean;
+  /** Whether the reply to this interaction has been deferred */
+  deferred: boolean;
+  /** Whether this interaction has already been replied to */
+  replied: boolean;
+  /** An associated interaction webhook, can be used to further interact with this interaction */
+  webhook: Webhook;
+  constructor(client: Client, data: APIMessageComponentSelectMenuInteraction) {
+    super(client);
+    this.webhook = new Webhook(client, { channel_id: data.channel_id, guild_id: data.guild_id, id: data.application_id, token: data.token, application_id: data.application_id });
+    this.parseData(data);
+  }
+
+  get customId() {
+    return this.data.data.custom_id;
+  }
+
+  /** The component which was interacted with */
+  get component() {
+    return this.message.components.flatMap(row => row.components).find(component => component.customId === this.customId);
+  }
+
+  /** The type of component which was interacted with */
+  get componentType() {
+    return this.data.data.component_type;
+  }
+
+  /** The selected options for this interaction */
+  get values() {
+    return this.data.data.values;
+  }
+
+  /** The {@link Channel} that the interaction belongs to */
+  get channel() {
+    return !this.channelId ? undefined : this.client.caches.channels.get(this.channelId);
+  }
+
+  /** The Id of the channel the interaction is in */
+  get channelId() {
+    return this.data.channel_id;
+  }
+
+  /** The time the interaction was created at */
+  get createdAt() {
+    return new Date(this.createdTimestamp);
+  }
+
+  /** The timestamp the interaction was created Id */
+  get createdTimestamp() {
+    return Snowflake.deconstruct(this.id);
+  }
+
+  /** The interaction's token */
+  get token() {
+    return this.data.token;
+  }
+
+  /** The interaction's Id */
+  get id() {
+    return this.data.id;
+  }
+
+  /** The interaction's type */
+  get type() {
+    return this.data.type;
+  }
+
+  /** The interaction's Id */
+  get user() {
+    if (this.data.member) {
+      return this.client.caches.users.get(this.data.member.user.id) ?? this.client.users.updateOrSet(this.data.member.user.id, this.data.member.user);
+    }
+    if (this.data.user) return this.client.caches.users.get(this.data.user.id) ?? this.client.users.updateOrSet(this.data.user.id, this.data.user);
+    return undefined;
+  }
+
+  get member() {
+    if (!this.guildId || !this.data.member) return undefined;
+    return this.client.caches.guilds.get(this.guildId)?.members.updateOrSet(this.data.member.user.id, this.data.member);
+  }
+
+  /** The message to which the component was attached */
+  get message() {
+    return this.client.messages.updateOrSet(this.data.message.id, this.data.message);
+  }
+
+  /** The locale of the user who invoked this interaction */
+  get locale() {
+    return this.data.locale;
+  }
+
+  /** The preferred locale from the guild this interaction was sent in */
+  get guildLocale() {
+    return this.data.guild_locale;
+  }
+
+  /** The Id of the guild the interaction is in */
+  get guildId() {
+    return this.data.guild_id ?? this.channel?.guild?.id;
+  }
+
+  /** The {@link Guild} that the interaction belongs to */
+  get guild() {
+    return !this.guildId ? undefined : this.client.caches.guilds.get(this.guildId) ?? this.channel?.guild;
+  }
+
+  /**
+   * Defers the reply to this interaction
+   * @param options Options for deferring the reply to this interaction
+   * @example
+   * // Defer the reply to this interaction
+   * interaction.deferReply()
+   * @example
+   * // Defer to send an ephemeral reply later
+   * interaction.deferReply({ ephemeral: true })
+   */
+  async deferReply(options?: InteractionDeferReplyOptions & { fetchReply: false }): Promise<undefined>;
+  async deferReply(options?: InteractionDeferReplyOptions & { fetchReply: true }): Promise<Message>;
+  async deferReply(options: InteractionDeferReplyOptions = {}) {
+    if (this.deferred || this.replied) throw new Error('This interaction has already been replied to');
+    this.ephemeral = options.ephemeral ?? false;
+    await this.client.rest.make(Routes.interactionCallback(this.id, this.token), 'Post', {
+      type: InteractionResponseType.DeferredChannelMessageWithSource,
+      data: {
+        flags: options.ephemeral ? MessageFlags.Ephemeral : undefined,
+      },
+    });
+    this.deferred = true;
+
+    return options.fetchReply ? this.fetchReply() : undefined;
+  }
+
+  /**
+   * Defers the update of this interaction
+   * @param options Options for deferring the update of this interaction
+   * @example
+   * // Defer the reply to this interaction
+   * interaction.deferUpdate()
+   * @example
+   * // Defer to send an ephemeral reply later
+   * interaction.deferUpdate({ ephemeral: true })
+   */
+  async deferUpdate(options?: InteractionDeferReplyOptions & { fetchReply: false }): Promise<undefined>;
+  async deferUpdate(options?: InteractionDeferReplyOptions & { fetchReply: true }): Promise<Message>;
+  async deferUpdate(options: InteractionDeferReplyOptions = {}) {
+    if (this.deferred || this.replied) throw new Error('This interaction has already been replied to');
+    await this.client.rest.make(Routes.interactionCallback(this.id, this.token), 'Post', { type: InteractionResponseType.DeferredMessageUpdate });
+    this.deferred = true;
+
+    return options.fetchReply ? this.fetchReply() : undefined;
+  }
+
+  /**
+   * Edits the initial reply to this interaction
+   * @param content The new options for the message
+   * @example
+   * ```js
+   * // Edit the original interaction message
+   * interaction.editReply(`Bot ping: ${client.ping}`)
+   * ```
+   * @example
+   * ```js
+   * // Edit the original interaction message and remove all embeds
+   * interaction.editReply({ content: 'Hey there!', embeds: [] })
+   * ```
+   */
+  async editReply(content: MessageOptions) {
+    if (!this.deferred && !this.replied) throw new Error('This interaction has not been replied to');
+    const message = await this.webhook.editMessage('@original', content);
+    this.replied = true;
+    return message;
+  }
+
+  /**
+   * Replies to the interaction
+   * @param content The options for the reply
+   * @example
+   * ```js
+   * // Reply to the interaction
+   * interaction.reply('Hello!')
+   * ```
+   * @example
+   * ```js
+   * // Reply to the interaction with an ephemeral reply
+   * interaction.reply({ content: 'Hey there!', ephemeral: true })
+   * ```
+   * @example
+   * ```js
+   * // Reply to the interaction with an ephemeral reply and fetch the reply
+   * const message = await interaction.reply({ content: 'Hey there!', ephemeral: true, fetchReply: true })
+   * console.log(message.content)
+   * ```
+   */
+  async reply(content: MessageOptions & InteractionDeferReplyOptions & { fetchReply: false }): Promise<undefined>;
+  async reply(content: MessageOptions & InteractionDeferReplyOptions & { fetchReply: true }): Promise<Message>;
+  async reply(content: MessageOptions & InteractionDeferReplyOptions) {
+    if (this.deferred || this.replied) throw new Error('This interaction has already been replied to');
+    this.ephemeral = content.ephemeral ?? false;
+    const parsedMessage = MakeAPIMessage.transform(content);
+
+    await this.client.rest.make(Routes.interactionCallback(this.id, this.token), 'Post', {
+      type: InteractionResponseType.ChannelMessageWithSource,
+      data: parsedMessage,
+    });
+    this.replied = true;
+
+    return content.fetchReply ? this.fetchReply() : undefined;
+  }
+
+  /**
+   * Updates the original message of the component on which the interaction was received on
+   * @param content The options for the updated message
+   * @example
+   * ```js
+   * // Reply to the interaction
+   * interaction.update('Hello!')
+   * ```
+   * @example
+   * ```js
+   * // Reply to the interaction with an ephemeral reply
+   * interaction.update({ content: 'Hey there!', ephemeral: true })
+   * ```
+   * @example
+   * ```js
+   * // Reply to the interaction with an ephemeral reply and fetch the reply
+   * const message = await interaction.update({ content: 'Hey there!', ephemeral: true, fetchReply: true })
+   * console.log(message.content)
+   * ```
+   */
+  async update(content: MessageOptions & InteractionDeferReplyOptions & { fetchReply: false }): Promise<undefined>;
+  async update(content: MessageOptions & InteractionDeferReplyOptions & { fetchReply: true }): Promise<Message>;
+  async update(content: MessageOptions & InteractionDeferReplyOptions) {
+    if (this.deferred || this.replied) throw new Error('This interaction has already been replied to');
+    this.ephemeral = content.ephemeral ?? false;
+    const parsedMessage = MakeAPIMessage.transform(content);
+
+    await this.client.rest.make(Routes.interactionCallback(this.id, this.token), 'Post', {
+      type: InteractionResponseType.UpdateMessage,
+      data: parsedMessage,
+    });
+    this.replied = true;
+
+    return content.fetchReply ? this.fetchReply() : undefined;
+  }
+
+  /**
+   * Send a follow-up message to this interaction
+   * @param content The options for the reply
+   */
+  followUp(options: MessageOptions & InteractionDeferReplyOptions) {
+    if (!this.deferred && !this.replied) throw new Error('This interaction has not been replied to');
+    return this.webhook.send(options);
+  }
+
+  /** Fetches the initial reply to this interaction */
+  deleteReply() {
+    if (this.ephemeral) throw new Error('Cannot delete an ephemeral reply');
+    return this.webhook.deleteMessage('@original');
+  }
+
+  /** Fetches the initial reply to this interaction */
+  fetchReply() {
+    return this.webhook.fetchMessage('@original');
+  }
+
+  /** @private */
+  parseData(data: APIMessageComponentSelectMenuInteraction, args: Partial<SelectMenuInteraction> = {}) {
+    this.data = { ...this.data, ...data };
+    this.ephemeral = args.ephemeral ?? false;
+    this.deferred = args.deferred ?? false;
+    this.replied = args.replied ?? false;
+    return this;
+  }
+}
+
+export { SelectMenuInteraction };
