b8ede91dee5acfdcf1898887719a422ad93ae773
Nicolas Ribeiro <psychoniskii2@gmail.com>
2022-05-16 14:11:49 -0300

    refactor: update types (#160)

--- src/client/actions/GUILD_UPDATE.ts
+++ src/client/actions/GUILD_UPDATE.ts
@@ -1,10 +1,10 @@
-import type { GatewayGuildUpdateDispatchData } from 'discord-api-types/v10';
+import type { GatewayGuildCreateDispatchData, GatewayGuildUpdateDispatchData } from 'discord-api-types/v10';
 import { Events } from '../../constants/Events';
 import type { Client } from '../Client';
 
 function handle(client: Client, data: GatewayGuildUpdateDispatchData) {
   const oldGuild = client.caches.guilds.get(data.id);
-  const newGuild = client.guilds.updateOrSet(data.id, data);
+  const newGuild = client.guilds.updateOrSet(data.id, data as GatewayGuildCreateDispatchData);
   if (client.ready) client.emit(Events.GuildUpdate, oldGuild, newGuild);
 }
 
