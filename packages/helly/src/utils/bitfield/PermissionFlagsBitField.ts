6411ccc0b386ff795ae880a5feacbdc4c39d7d00
davipatricio <davipatricio@pm.me>
2022-09-28 15:57:27 -0300

    fix compilation

--- packages/helly/src/utils/bitfield/PermissionFlagsBitField.ts
+++ packages/helly/src/utils/bitfield/PermissionFlagsBitField.ts
@@ -1,7 +1,7 @@
 import { PermissionFlagsBits } from 'discord-api-types/v10';
 import { BitField } from './BitField';
 
-export type IntentsResolvable = keyof typeof PermissionFlagsBits | number | PermissionBitField | (keyof typeof PermissionFlagsBits | number | PermissionBitField)[];
+export type PermissionResolvable = keyof typeof PermissionFlagsBits | number | PermissionBitField | (keyof typeof PermissionFlagsBits | number | PermissionBitField)[];
 
 export class PermissionBitField extends BitField {
   public static override Flags = PermissionFlagsBits;
