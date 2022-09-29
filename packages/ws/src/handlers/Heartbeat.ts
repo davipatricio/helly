4bef761bb1e376aa9d4febcd81ba581fc0f44a2c
davipatricio <davipatricio@pm.me>
2022-09-29 20:23:13 -0300

    start of handling disconnections

--- packages/ws/src/handlers/Heartbeat.ts
+++ packages/ws/src/handlers/Heartbeat.ts
@@ -1,9 +1,15 @@
+import { setTimeout as sleep } from 'timers/promises';
+
 import { GatewayOpcodes } from 'discord-api-types/v10';
 import type { WebSocketClient } from '../client/WebSocketClient';
 
 export function sendHeartbeat(client: WebSocketClient) {
+  client.data.lastHeartbeatAck = false;
+
   client.emit('Debug', 'Sending heartbeat to Discord.');
   client.send({ d: client.data.sequence, op: GatewayOpcodes.Heartbeat });
+
+  handlePossibleDisconnection(client);
 }
 
 export function handleHeartbeat(client: WebSocketClient) {
@@ -13,10 +19,7 @@ export function handleHeartbeat(client: WebSocketClient) {
 export function startHeartbeatInterval(client: WebSocketClient) {
   if (client.data.heartbeater) return;
 
-  client.data.heartbeater = setInterval(() => {
-    client.data.lastHeartbeatAck = false;
-    sendHeartbeat(client);
-  }, client.data.heartbeatInterval);
+  client.data.heartbeater = setInterval(() => sendHeartbeat(client), client.data.heartbeatInterval);
 }
 
 export function stopHeartbeatInterval(client: WebSocketClient) {
@@ -25,4 +28,23 @@ export function stopHeartbeatInterval(client: WebSocketClient) {
 
   clearInterval(client.data.heartbeater);
   client.data.heartbeater = null;
+}
+
+async function handlePossibleDisconnection(client: WebSocketClient) {
+  await sleep(10000);
+
+  if (client.data.lastHeartbeatAck) return;
+  stopHeartbeatInterval(client);
+
+  client.emit('Debug', 'Discord did not send a Heartbeat ACK in 10 seconds, waiting 5 more seconds.');
+
+  await sleep(5000);
+  if (client.data.lastHeartbeatAck) {
+    client.emit('Debug', 'Discord sent a Heartbeat ACK, resuming heartbeat.');
+    startHeartbeatInterval(client);
+    return;
+  }
+
+  client.emit('Debug', 'Discord did not send a Heartbeat ACK in 15 seconds. Attempting to reconnect.');
+  // TODO: Reconnect
 }
