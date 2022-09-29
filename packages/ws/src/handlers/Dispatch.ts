c6766c6ae94cfce50ac9ce364d0a36e6db001494
davipatricio <davipatricio@pm.me>
2022-09-29 20:27:07 -0300

    also store the sequence number

--- packages/ws/src/handlers/Dispatch.ts
+++ packages/ws/src/handlers/Dispatch.ts
@@ -4,6 +4,9 @@ import type { WebSocketClient } from '../client/WebSocketClient';
 export function handleDispatch(client: WebSocketClient, data: GatewayDispatchPayload) {
   client.emit('Dispatch', data);
 
+  // Store the sequence number
+  client.data.sequence = data.s;
+
   // Cache the resume gateway url
   if (data.t === 'READY') {
     client.data.resumeGatewayUrl = data.d.resume_gateway_url;
