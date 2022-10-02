362e2f1df77b8060b9750b94e2567e87609ca13d
davipatricio <davipatricio@pm.me>
2022-10-01 22:20:28 -0300

    remove unnecessary code

--- packages/helly/src/client/actions/Ready.ts
+++ packages/helly/src/client/actions/Ready.ts
@@ -3,7 +3,6 @@ import { IntentsBitField } from '../../utils';
 import type { Client } from '../Client';
 
 function handle(client: Client /* data: GatewayReadyDispatch */) {
-  const baseIntents = new IntentsBitField(client.options.intents);
   if (!client.ready) {
     setTimeout(
       () => {
@@ -11,7 +10,7 @@ function handle(client: Client /* data: GatewayReadyDispatch */) {
         client.emit('Ready');
       },
       // If the client has the GUILDS intent, wait X seconds before emitting ready otherwise emit it immediately
-      baseIntents.has(GatewayIntentBits.Guilds) ? client.options.waitGuildTimeout : 0,
+      client.options.intents.has(GatewayIntentBits.Guilds) ? client.options.waitGuildTimeout : 0,
     );
   }
 }
