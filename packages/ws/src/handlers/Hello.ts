cad0b3f875a1377f79a44ff8168fcafbb7b83647
davipatricio <davipatricio@pm.me>
2022-10-01 22:01:10 -0300

    fix heartbeats

--- packages/ws/src/handlers/Hello.ts
+++ packages/ws/src/handlers/Hello.ts
@@ -1,5 +1,6 @@
 import type { GatewayHelloData } from 'discord-api-types/v10';
 import type { WebSocketClient } from '../client/WebSocketClient';
+import { startHeartbeatInterval } from './Heartbeat';
 import { sendIdentify } from './Identify';
 import { sendResume } from './Resume';
 
@@ -7,6 +8,7 @@ export function handleHello(client: WebSocketClient, data: GatewayHelloData) {
   client.data.heartbeatInterval = data.heartbeat_interval;
 
   client.emit('Debug', 'Handling hello request from Discord.');
+  startHeartbeatInterval(client);
 
   if (client.data.sessionId && client.data.sequence && client.options.token) {
     client.emit('Debug', 'WebSocketClient has a session ID and sequence, attempting to resume.');
