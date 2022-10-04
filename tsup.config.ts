d964d589acf2bb5550d8c81b4f702b49f4f8cade
davipatricio <davipatricio@pm.me>
2022-10-03 21:32:56 -0300

    fix ts errors on tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,18 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
+  clean: true,
+  dts: true,
   entry: ['src/index.ts'],
   external: [],
+  format: ['esm', 'cjs'],
+  keepNames: true,
+  minify: false,
   noExternal: [],
   platform: 'node',
-  format: ['esm', 'cjs'],
-  target: 'es2022',
-  skipNodeModulesBundle: true,
-  clean: true,
   shims: true,
-  minify: false,
-  splitting: false,
-  keepNames: true,
-  dts: true,
+  skipNodeModulesBundle: true,
   sourcemap: true,
+  splitting: false,
+  target: 'es2022',
 });
