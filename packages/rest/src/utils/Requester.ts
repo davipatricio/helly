96795cec19b0bdf15284ee7ccdc7567b0ec69288
davipatricio <davipatricio@pm.me>
2022-10-03 22:12:04 -0300

    feat(rest): add request methods

--- packages/rest/src/utils/Requester.ts
+++ packages/rest/src/utils/Requester.ts
@@ -1,4 +1,8 @@
+import { RateLimit, RateLimitManager } from '@sapphire/ratelimits';
 import { RouteBases } from 'discord-api-types/v10';
+import { request } from 'undici';
+import type { DispatchOptions } from 'undici/types/agent';
+import type { HttpMethod } from 'undici/types/dispatcher';
 
 export interface Routes {
   api: string;
@@ -29,12 +33,16 @@ const DEFAULT_REQUESTER_OPTIONS: RequesterOptions = {
 
 export class Requester {
   options: RequesterOptions;
+  ratelimit: RateLimit;
+  ratelimitManager: RateLimitManager;
   constructor(options?: Partial<RequesterOptions>) {
     this.#checkOptions(options);
+    this.ratelimitManager = new RateLimitManager(5000);
+    this.ratelimit = new RateLimit(this.ratelimitManager);
   }
 
   #checkOptions(options?: Partial<RequesterOptions>) {
-    this.options = { ...options, ...DEFAULT_REQUESTER_OPTIONS };
+    this.options = { ...DEFAULT_REQUESTER_OPTIONS, ...options };
 
     if (this.options.authorization === '') {
       throw new Error('No authorization token provided');
@@ -43,5 +51,43 @@ export class Requester {
     if (!['Bot', 'Bearer'].includes(this.options.authorizationPrefix)) {
       throw new Error('Invalid authorization prefix');
     }
+  }
+
+  // https://github.com/discordjs/discord.js/blob/d9137c36580b0fd52ad1af99324fb1026b8752d9/packages/rest/src/lib/RequestManager.ts#L495
+  #getMajorParameter(endpoint: `/${string}`): string | 'global' {
+    const majorIdMatch = /^\/(?:channels|guilds|webhooks)\/(\d{16,19})/.exec(endpoint);
+
+    // Get the major id for this route - global otherwise
+    const majorId = majorIdMatch?.[1] ?? 'global';
+    return majorId;
+  }
+
+  request(endpoint: `/${string}`, method: HttpMethod = 'GET', body: DispatchOptions['body'] = undefined, additionalHeaders = {} as Record<string, string>) {
+    const majorParam = this.#getMajorParameter(endpoint);
+
+    const ratelimit = this.ratelimitManager.acquire(majorParam);
+    // TODO: rateLimited event
+    if (ratelimit.limited) {
+      // Queue the request
+      setTimeout(() => {
+        this.request(endpoint, method, body, additionalHeaders);
+      }, ratelimit.remainingTime);
+      return;
+    }
+
+    ratelimit.consume();
+
+    const url = `${this.options.routes.api ?? RouteBases.api}${endpoint}`;
+    const headers = {
+      Authorization: `${this.options.authorizationPrefix} ${this.options.authorization}`,
+      'User-Agent': 'DiscordBot (https://github.com/davipatricio/helly, v0.0.1)',
+      ...additionalHeaders,
+    };
+
+    request(url, {
+      body,
+      headers,
+      method,
+    });
   }
 }
